e5625889272c507480db419bd5f9d58a591cef5d
muhshi <muhshi>
2025-12-05 09:33:49 +0700

    memberikan akses ke user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Allow all users to access the panel (or add logic: return $this->email === 'admin@gmail.com';)
+    }
 
     /**
      * The attributes that are mass assignable.
